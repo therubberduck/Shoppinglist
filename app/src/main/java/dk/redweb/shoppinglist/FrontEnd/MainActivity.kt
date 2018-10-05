29e0ba9f7ced00e50dc23eef1960ddaab383f5bb
Thorbjørn Steen <thorb@redweb.dk>
2018-10-05 14:58:43 +0200

    Remove Firebase Crashlytics while in debug mode

--- app/src/main/java/dk/redweb/shoppinglist/FrontEnd/MainActivity.kt
+++ app/src/main/java/dk/redweb/shoppinglist/FrontEnd/MainActivity.kt
@@ -1,12 +1,14 @@
 package dk.redweb.shoppinglist.FrontEnd
 
 import android.os.Bundle
+import android.os.Debug
 import com.crashlytics.android.Crashlytics
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.wealthfront.magellan.Navigator
 import com.wealthfront.magellan.Screen
 import com.wealthfront.magellan.support.SingleActivity
 import dk.redweb.shoppinglist.App
+import dk.redweb.shoppinglist.BuildConfig
 import dk.redweb.shoppinglist.FrontEnd.MainView.FilterListFragment.FilterListScreen
 import dk.redweb.shoppinglist.FrontEnd.MainView.MainScreen
 import dk.redweb.shoppinglist.FrontEnd.MainView.OnListFragment.OnListScreen
@@ -41,7 +43,9 @@ class MainActivity : SingleActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
 
-        _firebaseAnalytics = FirebaseAnalytics.getInstance(this) //TODO Ask user for permission?
-        Fabric.with(this, Crashlytics()) //TODO Turns on Firebase Crashlytics. Ask for user confirmation first
+        if(!BuildConfig.DEBUG) {
+            _firebaseAnalytics = FirebaseAnalytics.getInstance(this) //TODO Ask user for permission?
+            Fabric.with(this, Crashlytics()) //TODO Turns on Firebase Crashlytics. Ask for user confirmation first
+        }
     }
 }
